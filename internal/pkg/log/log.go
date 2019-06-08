b5255e4e4514781bff0358b588d4eb3d367022fd
John Nguyen <jtnguyen236@gmail.com>
2019-06-08 12:22:20 +0800

    remove singleton pattern from logger

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -2,191 +2,153 @@ package log
 
 import (
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-var z *zap.SugaredLogger
+// Logger is a single logger instance
+type Logger struct {
+	*zap.SugaredLogger
+}
 
-func NewToFile(filename string) *zap.SugaredLogger {
+// New creates a new logger that writes to a file
+func New() *Logger {
+	conf := zap.NewDevelopmentConfig()
+	l, err := conf.Build(
+		zap.AddStacktrace(zap.FatalLevel),
+	)
+	if err != nil {
+		panic(err)
+	}
+	z := l.Sugar()
+	return &Logger{z}
+}
+
+// NewToFile creates a new logger that writes to a file
+func NewToFile(filename string) *Logger {
 	conf := zap.NewDevelopmentConfig()
 	conf.OutputPaths = []string{
 		filename,
 	}
 	l, err := conf.Build(
-		zap.AddCallerSkip(1),
 		zap.AddStacktrace(zap.FatalLevel),
 	)
 	if err != nil {
 		panic(err)
 	}
-	z = l.Sugar()
-	return z
-}
-func New(production bool) *zap.SugaredLogger {
-	conf := zap.NewDevelopmentConfig()
-	conf.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	l, err := conf.Build(
-		zap.AddCallerSkip(1),
-		zap.AddStacktrace(zap.FatalLevel),
-	)
-
-	if production {
-		l, err = zap.NewProduction(
-			zap.AddCallerSkip(1),
-			zap.AddStacktrace(zap.FatalLevel),
-		)
-	}
-
-	if err != nil {
-		panic(err)
-	}
-
-	z = l.Sugar()
-	return z
-}
-
-// Get returns the package global
-func Get() *zap.SugaredLogger {
-	if z == nil {
-		panic("log not initialized")
-	}
-	return z
+	z := l.Sugar()
+	return &Logger{z}
 }
 
 // Fatal is a wrapper for zap's Fatal
-func Fatal(args ...interface{}) {
-	z.Fatal(args...)
+func (z *Logger) Fatal(args ...interface{}) {
+	z.SugaredLogger.Fatal(args...)
 }
 
 // Print is a wrapper for zap's Print
-func Print(args ...interface{}) {
-	z.Info(args...)
+func (z *Logger) Print(args ...interface{}) {
+	z.SugaredLogger.Info(args...)
 }
 
 // Info is a wrapper for zap's Info
-func Info(args ...interface{}) {
-	z.Info(args...)
+func (z *Logger) Info(args ...interface{}) {
+	z.SugaredLogger.Info(args...)
 }
 
 // Warn is a wrapper for zap's Warn
-func Warn(args ...interface{}) {
-	z.Warn(args...)
+func (z *Logger) Warn(args ...interface{}) {
+	z.SugaredLogger.Warn(args...)
 }
 
 // Error is a wrapper for zap's Error
-func Error(args ...interface{}) {
-	z.Error(args...)
+func (z *Logger) Error(args ...interface{}) {
+	z.SugaredLogger.Error(args...)
 }
 
 // Debug is a wrapper for zap's Debug
-func Debug(args ...interface{}) {
-	z.Debug(args...)
+func (z *Logger) Debug(args ...interface{}) {
+	z.SugaredLogger.Debug(args...)
 }
 
 // Fatalln is a wrapper for zap's Fatalln
-func Fatalln(args ...interface{}) {
-	z.Fatal(args...)
+func (z *Logger) Fatalln(args ...interface{}) {
+	z.SugaredLogger.Fatal(args...)
 }
 
 // Println is a wrapper for zap's Println
-func Println(args ...interface{}) {
-	z.Info(args...)
+func (z *Logger) Println(args ...interface{}) {
+	z.SugaredLogger.Info(args...)
 }
 
 // Infoln is a wrapper for zap's Infoln
-func Infoln(args ...interface{}) {
-	z.Info(args...)
+func (z *Logger) Infoln(args ...interface{}) {
+	z.SugaredLogger.Info(args...)
 }
 
 // Warnln is a wrapper for zap's Warnln
-func Warnln(args ...interface{}) {
-	z.Warn(args...)
+func (z *Logger) Warnln(args ...interface{}) {
+	z.SugaredLogger.Warn(args...)
 }
 
 // Errorln is a wrapper for zap's Errorln
-func Errorln(args ...interface{}) {
-	z.Error(args...)
+func (z *Logger) Errorln(args ...interface{}) {
+	z.SugaredLogger.Error(args...)
 }
 
 // Debugln is a wrapper for zap's Debugln
-func Debugln(args ...interface{}) {
-	z.Debug(args...)
+func (z *Logger) Debugln(args ...interface{}) {
+	z.SugaredLogger.Debug(args...)
 }
 
 // Fatalw is a wrapper for zap's Fatalw
-func Fatalw(msg string, keysandvalues ...interface{}) {
-	z.Fatalw(msg, keysandvalues...)
+func (z *Logger) Fatalw(msg string, keysandvalues ...interface{}) {
+	z.SugaredLogger.Fatalw(msg, keysandvalues...)
 }
 
 // Printw is a wrapper for zap's Printw
-func Printw(msg string, keysandvalues ...interface{}) {
-	z.Infow(msg, keysandvalues...)
+func (z *Logger) Printw(msg string, keysandvalues ...interface{}) {
+	z.SugaredLogger.Infow(msg, keysandvalues...)
 }
 
 // Infow is a wrapper for zap's Infow
-func Infow(msg string, keysandvalues ...interface{}) {
-	z.Infow(msg, keysandvalues...)
+func (z *Logger) Infow(msg string, keysandvalues ...interface{}) {
+	z.SugaredLogger.Infow(msg, keysandvalues...)
 }
 
 // Warnw is a wrapper for zap's Warnw
-func Warnw(msg string, keysandvalues ...interface{}) {
-	z.Warnw(msg, keysandvalues...)
+func (z *Logger) Warnw(msg string, keysandvalues ...interface{}) {
+	z.SugaredLogger.Warnw(msg, keysandvalues...)
 }
 
 // Errorw is a wrapper for zap's Errorw
-func Errorw(msg string, keysandvalues ...interface{}) {
-	z.Errorw(msg, keysandvalues...)
+func (z *Logger) Errorw(msg string, keysandvalues ...interface{}) {
+	z.SugaredLogger.Errorw(msg, keysandvalues...)
 }
 
 // Debugw is a wrapper for zap's Debugw
-func Debugw(msg string, keysandvalues ...interface{}) {
-	z.Debugw(msg, keysandvalues...)
+func (z *Logger) Debugw(msg string, keysandvalues ...interface{}) {
+	z.SugaredLogger.Debugw(msg, keysandvalues...)
 }
 
 // Printf is a wrapper for zap's Printf
-func Printf(format string, args ...interface{}) {
-	z.Infof(format, args...)
+func (z *Logger) Printf(format string, args ...interface{}) {
+	z.SugaredLogger.Infof(format, args...)
 }
 
 // Infof is a wrapper for zap's Infof
-func Infof(format string, args ...interface{}) {
-	z.Infof(format, args...)
+func (z *Logger) Infof(format string, args ...interface{}) {
+	z.SugaredLogger.Infof(format, args...)
 }
 
 // Warnf is a wrapper for zap's Warnf
-func Warnf(format string, args ...interface{}) {
-	z.Warnf(format, args...)
+func (z *Logger) Warnf(format string, args ...interface{}) {
+	z.SugaredLogger.Warnf(format, args...)
 }
 
 // Errorf is a wrapper for zap's Errorf
-func Errorf(format string, args ...interface{}) {
-	z.Errorf(format, args...)
+func (z *Logger) Errorf(format string, args ...interface{}) {
+	z.SugaredLogger.Errorf(format, args...)
 }
 
 // Debugf is a wrapper for zap's Debugf
-func Debugf(format string, args ...interface{}) {
-	z.Debugf(format, args...)
-}
-
-// Fields wraps logrus fields
-type Fields map[string]interface{}
-
-// WithFields is a wrapper for zap's WithFields
-func WithFields(fields Fields) *Entry {
-	// entry := logrus.WithFields(logrus.Fields(fields))
-	for k, v := range fields {
-		z.With(k, v)
-	}
-	return &Entry{z}
-}
-
-// Entry wraps the logrus Entry type
-type Entry struct {
-	*zap.SugaredLogger
-}
-
-// WithField is a wrapper for zap's WithField
-func WithField(key string, value interface{}) *Entry {
-	z.With(key, value)
-	return &Entry{z}
+func (z *Logger) Debugf(format string, args ...interface{}) {
+	z.SugaredLogger.Debugf(format, args...)
 }
